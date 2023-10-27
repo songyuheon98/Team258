a4ea8a8db8c0f140560179351a65e17db463ba05
INYONG KIM <ohnyong87@gmail.com>
2023-10-27 19:57:17 +0900

    feat : 관리자 - 도서 조회기능 JPQL로 서비스계층 구현 버전 코드

--- src/main/java/com/example/team258/domain/admin/service/AdminBooksService.java
+++ src/main/java/com/example/team258/domain/admin/service/AdminBooksService.java
@@ -1,29 +1,24 @@
 package com.example.team258.domain.admin.service;
 
 import com.example.team258.common.dto.BooksPageResponseDto;
-import com.example.team258.common.entity.*;
+import com.example.team258.common.dto.MessageDto;
+import com.example.team258.common.entity.Book;
+import com.example.team258.common.entity.BookCategory;
+import com.example.team258.common.entity.User;
+import com.example.team258.common.entity.UserRoleEnum;
 import com.example.team258.domain.admin.dto.AdminBooksRequestDto;
 import com.example.team258.domain.admin.dto.AdminBooksResponseDto;
-import com.example.team258.common.dto.MessageDto;
 import com.example.team258.domain.admin.repository.AdminBooksRepository;
 import com.example.team258.domain.admin.repository.BookCategoryRepository;
-//import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.BooleanBuilder;
-import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
-//import com.querydsl.core.types.dsl.BooleanExpression;
-//import static com.example.QBook.book; // QBook 클래스의 정적 임포트
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -32,6 +27,10 @@ import java.util.List;
 public class AdminBooksService {
     private final AdminBooksRepository adminBooksRepository;
     private final BookCategoryRepository bookCategoryRepository;
+
+    // JPQL 용 엔티티 매니저
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Transactional
     public MessageDto createBook(AdminBooksRequestDto requestDto, User loginUser) {
@@ -55,25 +54,73 @@ public class AdminBooksService {
         }
     }
 
+    //public BooksPageResponseDto findBooksWithPaginationAndSearching(User loginUser, String keyword, Pageable pageable) {
+    //    // 로그인한 사용자 관리자 확인
+    //    validateUserAuthority(loginUser);
+    //
+    //    QBook qBook = QBook.book;
+    //    BooleanBuilder builder = new BooleanBuilder();
+    //
+    //    // 검색어가 있을 경우 검색 조건을 추가
+    //    if (StringUtils.hasText(keyword))
+    //        builder.and(qBook.bookName.containsIgnoreCase(keyword));
+    //
+    //    // 페이징된 엔티티를 Dto로 변환하여 반환
+    //    Page<Book> adminBooks = adminBooksRepository.findAll(builder, pageable);
+    //    int totalPages = adminBooks.getTotalPages();
+    //    List<AdminBooksResponseDto> booksResponseDtos = adminBooks.stream().map(AdminBooksResponseDto::new).toList();
+    //
+    //    return new BooksPageResponseDto(booksResponseDtos, totalPages);
+    //}
+
+    // 검색페이징 조회 부분의 JPQL 구현
     public BooksPageResponseDto findBooksWithPaginationAndSearching(User loginUser, String keyword, Pageable pageable) {
         // 로그인한 사용자 관리자 확인
         validateUserAuthority(loginUser);
 
-        QBook qBook = QBook.book;
-        BooleanBuilder builder = new BooleanBuilder();
+        // JPQL 쿼리 문자열
+        String jpql = "SELECT b FROM book b WHERE 1 = 1";
 
-        // 검색어가 있을 경우 검색 조건을 추가
+        // 검색어가 주어진 경우, 도서명에 대한 검색 조건 추가
         if (StringUtils.hasText(keyword))
-            builder.and(qBook.bookName.containsIgnoreCase(keyword));
+            jpql += " AND LOWER(b.bookName) LIKE LOWER(:keyword)";
 
-        // 페이징된 엔티티를 Dto로 변환하여 반환
-        Page<Book> adminBooks = adminBooksRepository.findAll(builder, pageable);
-        int totalPages = adminBooks.getTotalPages();
-        List<AdminBooksResponseDto> booksResponseDtos = adminBooks.stream().map(AdminBooksResponseDto::new).toList();
+        // JPQL 쿼리 실행
+        TypedQuery<Book> query = entityManager.createQuery(jpql, Book.class);
 
+        // 파라미터 바인딩
+        if (StringUtils.hasText(keyword))
+            query.setParameter("keyword", "%" + keyword + "%");
+
+        // 페이징 처리
+        query.setFirstResult((int) pageable.getOffset());
+        query.setMaxResults(pageable.getPageSize());
+
+        // 조회된 엔티티를 Dto로 변환
+        List<AdminBooksResponseDto> booksResponseDtos = query.getResultList().stream()
+                .map(AdminBooksResponseDto::new)
+                .toList();
+
+        // 전체 페이지 수 계산
+        int totalPages = (int) Math.ceil((double) getTotalBooksCount(keyword) / pageable.getPageSize());
+
+        // 결과를 포함한 응답 Dto 반환
         return new BooksPageResponseDto(booksResponseDtos, totalPages);
     }
 
+    // 검색어에 따른 전체 도서 개수 조회
+    private Long getTotalBooksCount(String keyword) {
+        String countJpql = "SELECT COUNT(b) FROM book b WHERE 1 = 1";
+        if (StringUtils.hasText(keyword))
+            countJpql += " AND LOWER(b.bookName) LIKE LOWER(:keyword)";
+
+        TypedQuery<Long> countQuery = entityManager.createQuery(countJpql, Long.class);
+
+        if (StringUtils.hasText(keyword))
+            countQuery.setParameter("keyword", "%" + keyword + "%");
+
+        return countQuery.getSingleResult();
+    }
 
     @Transactional
     public MessageDto updateBook(AdminBooksRequestDto requestDto, Long bookId, User loginUser) {
