666887f022735db8f0092868c50bd49ca5f73e84
INYONG KIM <ohnyong87@gmail.com>
2023-11-01 19:37:20 +0900

    feat : 페이지 더보기와 검색, 대여, 예약 기능 연결 / 검색 키워드카테고리 쿼리유지

--- src/main/resources/static/js/users/bookSearchLM1.js
+++ src/main/resources/static/js/users/bookSearchLM1.js
@@ -2,19 +2,48 @@
 
 let currentPage = 0;
 
+function search() {
+    let keyword = document.getElementById('keyword').value;
+    let category = document.getElementById('bookCategoryId').value;
+
+    if(keyword != '' & category != '')
+        url = `/search/lm1?keyword=${keyword}&bookCategoryName=${category}&page=0`
+    else if(keyword == '' & category !='')
+        url = `/search/lm1?bookCategoryName=${category}&page=0`
+    else if(keyword != '' & category =='')
+        url = `/search/lm1?keyword=${keyword}&page=0`
+    else if(keyword == '' & category =='')
+        url = "/search/lm1?page=0"
+
+    window.location.href = url;
+}
 // 페이지 로딩 시
 $(document).ready(function() {
     window.goToNextPage = function() {
+        // 현재 페이지 업데이트
+        currentPage++;
+
+        // 현재 URL에서 keyword와 bookCategoryName 값을 가져오기
+        let urlParams = new URLSearchParams(window.location.search);
+        let keyword = urlParams.get('keyword');
+        let category = urlParams.get('bookCategoryName');
+
+        // URL 생성
+        let url = '/search/loadMore?page=' + currentPage;
+
+        // 검색어 및 카테고리 정보가 있다면 추가
+        if (keyword) {
+            url += '&keyword=' + keyword;
+        }
+        if (category) {
+            url += '&bookCategoryName=' + category;
+        }
+
         // 서버에 다음 페이지 데이터 요청
         $.ajax({
             type: 'GET',
-            url: '/search/loadMore', // 엔드포인트 변경
-            data: {
-                bookCategoryName: $('#bookCategoryId').val(),
-                keyword: $('#keyword').val(),
-                page: currentPage + 1 // 다음 페이지로 이동
-            },
-            dataType: 'json', // 추가
+            url: url,
+            dataType: 'json',
 
             success: function(data) {
                 console.log(data);
@@ -46,6 +75,56 @@ $(document).ready(function() {
                 // 오류 처리 로직
                 console.error(error);
             }
+        });
+    }
+
+
+    // 대출하기 버튼 클릭 시 이벤트 핸들러 설정
+    $(document).on('click', '.rent-button', function () {
+        const bookId = $(this).data('book-id');
+        rentBook(bookId);
+    });
+
+
+    // 예약하기 버튼 클릭 시 이벤트 핸들러 설정
+    $(document).on('click', '.reserve-button', function () {
+        const bookId = $(this).data('book-id');
+        reserveBook(bookId);
+    });
+
+
+
+    function rentBook(bookId) {
+        $.ajax({
+            url: `/api/books/${bookId}/rental`,
+            type: "POST",
+            success: function (response) {
+                alert("대여가 완료되었습니다.");
+                //현재 성능상 문제로 reload는 안함
+                window.location.reload();
+            },
+            error: function (error) {
+                alert("대여에 실패했습니다.");
+                console.error(error);
+                //추후 오류 문구 전달 필요?
+            },
+        });
+    }
+
+    function reserveBook(bookId) {
+        $.ajax({
+            url: `/api/books/${bookId}/reservation`,
+            type: "POST",
+            success: function (response) {
+                alert("예약이 완료되었습니다.");
+                //현재 성능상 문제로 reload는 안함
+                window.location.reload();
+            },
+            error: function (error) {
+                alert("예약에 실패했습니다.");
+                console.error(error);
+                //추후 오류 문구 전달 필요?
+            },
         });
     }
 });
