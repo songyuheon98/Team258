9079804bf16a35031552be2dd1f85d54a58ac899
INYONG KIM <ohnyong87@gmail.com>
2023-11-06 11:18:49 +0900

    feat : ElasticsearchConfig 작성 연결 설정

--- src/main/java/com/example/team258/common/config/ElasticSearchOperations.java
+++
@@ -1,8 +0,0 @@
-package com.example.team258.common.config;
-
-import org.springframework.data.elasticsearch.core.DocumentOperations;
-import org.springframework.data.elasticsearch.core.SearchOperations;
-
-public interface ElasticSearchOperations extends DocumentOperations, SearchOperations {
-    //...
-}
---
+++ src/main/java/com/example/team258/common/config/ElasticsearchConfig.java
@@ -0,0 +1,20 @@
+package com.example.team258.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elastic.url}")
+    private String elasticUrl;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticUrl)
+                .build();
+    }
+}
