587b4a8ee441b6cff1d4ce71fac31b14ff622ff6
INYONG KIM <ohnyong87@gmail.com>
2023-11-06 22:51:10 +0900

    feat : Elasticsearch+무한스크롤구현

--- src/main/java/com/example/team258/domain/elasticsearch/service/ElasticBookService.java
+++ src/main/java/com/example/team258/domain/elasticsearch/service/ElasticBookService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,10 +24,14 @@ public class ElasticBookService {
     private final BookRepository bookRepository;
     private final ElasticBookSearchRepositoryElastic elasticBookSearchRepository;
 
-    public List<ElasticBookResponseDto> searchByBookName(String keyword, int page) {
-
+    public List<ElasticBookResponseDto> searchByBookName(String keyword, int page, int size) {
+        if (keyword == null) {
+            // 검색 키워드가 null인 경우에 대한 처리
+            // 예를 들어, 빈 리스트를 반환하거나 다른 처리를 수행할 수 있습니다.
+            return Collections.emptyList();
+        }
         Sort sort = Sort.by(Sort.Direction.ASC, "_id");
-        Pageable pageable = PageRequest.of(page, 10, sort);
+        Pageable pageable = PageRequest.of(page, size, sort);
 
         return elasticBookSearchRepository.findByBookNameContains(keyword, pageable)
             .stream()
