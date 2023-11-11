662c944c75b43705adb9185977f98e8660e28565
INYONG KIM <ohnyong87@gmail.com>
2023-11-11 15:13:13 +0900

    refactor : 엘라스틱템플릿 내 주소 중복표현 제거

--- src/main/java/com/example/team258/domain/elasticsearch/config/ElasticSearchConfig.java
+++ src/main/java/com/example/team258/domain/elasticsearch/config/ElasticSearchConfig.java
@@ -3,6 +3,7 @@ package com.example.team258.domain.elasticsearch.config;
 import com.example.team258.domain.elasticsearch.repository.ElasticBookSearchRepository;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -16,6 +17,9 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackageClasses = {ElasticBookSearchRepository.class})
 @Configuration
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+    @Value("${elasticsearch.hosts}")
+    private String elasticsearchHosts;
+
     @Bean
     public ElasticsearchOperations customElasticsearchTemplate(@Qualifier("elasticsearchClient") RestHighLevelClient client) {
         return new ElasticsearchRestTemplate(client);
@@ -23,13 +27,9 @@ public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                //.connectedTo("localhost:9200")
-                .connectedTo("43.200.91.27:9200") // EC2 인스턴스의 퍼블릭 IPv4 주소로 변경
+                .connectedTo(elasticsearchHosts) // application.properties에서 주석변경으로 로컬<->EC2 변경가능
                 .build();
         return RestClients.create(clientConfiguration).rest();
-        // org.springframework.data.elasticsearch.client
-        //    id 'org.springframework.boot' version '2.5.5'
-        //    implementation 'org.springframework.data:spring-data-elasticsearch:4.2.2'
     }
 
 }
